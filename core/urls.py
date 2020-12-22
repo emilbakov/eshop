ec4cf1c21b048e59cd8256897df86d7d2595ff65
unknown <eb.bakov@gmail.com>
2020-12-22 20:49:53 +0200

    add OrderSummary

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from .views import (
     checkout,
     HomeView,
     add_to_cart,
+    OrderSummeryView,
     remove_from_cart
 )
 
@@ -14,6 +15,7 @@ app_name = 'core'
 urlpatterns = [
     path('', HomeView.as_view() , name='home'),
     path('checkout/', checkout , name='checkout'),
+    path('order-summary/', OrderSummeryView.as_view() , name='order-summary'),
     path('product/<slug>/', ItemDetailView.as_view() , name='product'),
     path('add-to-cart/<slug>/', add_to_cart , name='add-to-cart'),
     path('remove-from-cart/<slug>/', remove_from_cart , name='remove-from-cart')
--- core/views.py
+++ core/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-from django.views.generic import ListView,DetailView
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+ 
+from django.views.generic import ListView,DetailView,View
 from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Item, OrderItem, Order
 
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
@@ -24,11 +26,28 @@ class HomeView(ListView):
     template_name='home-page.html'
 
 
+class OrderSummeryView(LoginRequiredMixin,View):
+    def get(self, *args, **kwargs):
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            context={
+               'object': order
+            }            
+            return render(self.request,'order_summary.html', context)
+                        
+        except ObjectDoesNotExist:
+            messages.error(self.request,"You do not have an active order")
+            return redirect("/")      
+       
+        
+       
+
+
 class ItemDetailView(DetailView):
     model = Item
     template_name='product-page.html'
 
-
+@login_required
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_item,created = OrderItem.objects.get_or_create(
@@ -55,6 +74,8 @@ def add_to_cart(request, slug):
         messages.info(request, "This item was added to your cart.")
     return redirect("core:product", slug=slug)           
 
+
+@login_required
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_qs = Order.objects.filter(
